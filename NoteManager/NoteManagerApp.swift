59bbf9356be4da2f204fb9b045c6a783e5bbb49e
Tunahn3101 <phamtuananh31012001@gmail.com>
2026-04-15 16:57:51 +0700

    Initial Commit

---
+++ NoteManager/NoteManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteManagerApp.swift
+//  NoteManager
+//
+//  Created by Tunahn on 15/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
