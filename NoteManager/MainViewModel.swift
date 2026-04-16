2d0d234e80deca8ca323a8d4eb39e47e61757c84
Tunahn3101 <phamtuananh31012001@gmail.com>
2026-04-16 18:28:13 +0700

    update

--- NoteManager/MainViewModel.swift
+++ NoteManager/MainViewModel.swift
@@ -5,3 +5,20 @@
 //  Created by Tunahn on 15/4/26.
 //
 
+import SwiftUI
+import Combine
+
+@MainActor
+class MainViewModel: ObservableObject {
+    @Published var msg : String = ""
+    @Published var isShowError  = false
+
+
+    
+    func checkShowError(msg: String){
+        if isShowError == false && msg.isEmpty == false {
+            self.msg = msg
+            isShowError = true
+        }
+    }
+}
